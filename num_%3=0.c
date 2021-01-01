79a1d3382be3544dc7d818fb0f5b399dbb98f0cd
s55669874 <76258420+s55669874@users.noreply.github.com>
2021-01-01 14:26:57 +0800

    Add files via upload

---
+++ num_%3=0.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    while(scanf("%d", &num)== 1)
+    {
+        int counter = 0;
+        int num_division[num];
+        for(int i = 1; i <= num ; i++)
+        {
+            if(i%3 == 0)
+                {
+                    num_division[counter] = i;
+                    counter++;
+                }
+        }
+        int total = 0;
+        for( int i = 0; i < counter; i++)
+        {
+            total += num_division[i];
+        }
+        printf("%d\n", total);
+    }
+    return 0;
+}
