4679589a83ba6dac222d7b34b246b0215ff5e3b9
s55669874 <76258420+s55669874@users.noreply.github.com>
2021-01-01 14:15:39 +0800

    Add files via upload

---
+++ is_priime.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    while( scanf("%d", &num) ==  1)
+    {
+        if(num%2 == 0 || num%3 == 0)
+        {
+            printf("NO\n");
+            continue;
+        }
+        for( int i = 2; i < num; i++)
+        {
+            if(i%2 == 0 || i%3 ==0)
+                continue;
+            else if(num%i == 0)
+            {
+                printf("NO\n");
+                break;
+            }
+            else
+            {
+                printf("YES\n");
+                break;
+            }
+        }
+    }
+    return 0;
+}
