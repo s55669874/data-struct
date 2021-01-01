5a922b11946076c9c403d0a8115922d455da70d3
s55669874 <76258420+s55669874@users.noreply.github.com>
2021-01-01 14:34:08 +0800

    Add files via upload

---
+++ Euclidean.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+int main()
+{
+    int num1, num2;
+    scanf("%d %d", &num1, &num2);
+    int answer;
+    while(num2 != 0)
+    {
+        answer = num2;
+        num2 = num1%num2;
+        num1 = answer;
+    }
+    printf("%d\n", answer);
+    return 0;
+}
