9f90ff0389c2f4fbae9804d37991a403083a2dc1
s55669874 <76258420+s55669874@users.noreply.github.com>
2021-01-01 14:04:07 +0800

    Add files via upload

---
+++ plural_calculus.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    int num;
+    int a,b,c,d;
+    char do_what;
+    scanf("%d",&num);
+    for(int i=0; i<num; i++)
+    {
+        scanf(" %c %d %d %d %d", &do_what,& a, &b, &c, &d);
+        switch(do_what)
+        {
+            case '+':
+                printf("%d %d\n",a+c,b+d);
+                break;
+            case '-':
+                printf("%d %d\n",a-c,b-d);
+                break;
+            case '*':
+                printf("%d %d\n",a*c-b*d,b*c+a*d);
+                break;
+        }
+    }
+}
