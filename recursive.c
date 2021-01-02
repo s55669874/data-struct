448c1e2028ac8437defdc29d15fec0fb28c579bb
s55669874 <76258420+s55669874@users.noreply.github.com>
2021-01-02 13:04:28 +0800

    Add files via upload

---
+++ recursive.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int recursive(int a)
+{
+    if(a == 0 || a == 1)
+        return a+1;
+    else
+        return recursive(a - 1) + recursive(a / 2);
+}
+int main()
+{
+    int num;
+    scanf("%d", &num);
+
+    int answer = recursive(num);
+    printf("%d\n", answer);
+    return 0;
+}
