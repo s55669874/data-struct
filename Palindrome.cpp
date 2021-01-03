926e333ab1cbc34ddb424e57e2ae1fd223ab9972
s55669874 <76258420+s55669874@users.noreply.github.com>
2021-01-03 20:48:56 +0800

    Add files via upload

---
+++ Palindrome.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    string num;
+    while(cin >> num)
+    {
+        string temp = num;
+        reverse(temp.begin(), temp.end());
+        if(num == temp)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+    return 0;
+}
