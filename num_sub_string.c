acbfd120e247436c33e9e163a758542ce22244ba
s55669874 <76258420+s55669874@users.noreply.github.com>
2021-01-03 22:30:20 +0800

    Add files via upload

---
+++ num_sub_string.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+    char sub_string[128], str[512];
+    scanf(" %s %s", sub_string, str);
+    int ans = 0, mark = 0;
+    for(int i = 0; i < strlen(str); i++)
+    {
+        mark = 0;
+        for(int j = 0; j < strlen(sub_string); j++)
+            {
+                if(str[i + j] == sub_string[j])
+                    mark = 1;
+                else
+                {
+                    mark = 0;
+                    break;
+                }
+            }
+            if(mark == 1)
+                ans++;
+    }
+    printf("%d\n", ans);
+    return 0;
+}
