b867f209be1d4ba2541369bd9c3faea583f53b71
s55669874 <76258420+s55669874@users.noreply.github.com>
2021-01-04 23:28:43 +0800

    Add files via upload

---
+++ judge_year.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int year;
+    while(scanf("%d", &year) == 1)
+    {
+        if(( year % 400 ) == 0)
+            printf("Bissextile Year\n");
+        else if((year % 4) == 0 && (year % 100) != 0)
+            printf("Bissextile Year\n");
+        else
+            printf("Common Year\n");
+    }
+}
