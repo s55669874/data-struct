44c49161bc8debf676b47081f163772b45f5a3a2
s55669874 <76258420+s55669874@users.noreply.github.com>
2021-01-04 22:01:00 +0800

    Add files via upload

---
+++ Compound_interest.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    long double sum = 0.0, r;
+    long long int n, p;
+    scanf("%Lf %lld %lld", &r, &n, &p);
+    //printf("%lf", r);
+    for(int i = 0; i < n; i++)
+    {
+        sum = sum + p;
+        sum = sum * (1.0 + r);
+    }
+    long long int ans = sum;
+    printf("%lld\n", ans);
+}
